3797e3d0e27a719d34cf520848c556853f7f7531
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-02-25 21:04:15 +0100

    Add pokemonList variable
    - use of array and object data
    - list of 4 pokemon
    - task 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favoriteFood='Apple';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', height:0.7, types:['grass', 'poison']},
+    {name: 'Charmander', height: 0.6, types: 'fire'},
+    {name:'Pikachu', height:0.4, types:'electric'},
+    {name:'Ponyta', height:1, types:'fire'},
+    ];
