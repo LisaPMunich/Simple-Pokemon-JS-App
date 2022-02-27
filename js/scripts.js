3b1d8748169b98ec6a77b53ae76d4ff1be10d023
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-02-27 08:59:09 +0100

    add css styling to app and add pokemon list
    - css font
    - basic structure (header, main)
    - js write pokemonList to DOM
    - highlight biggest pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
+"use strict";
+
 let pokemonList = [
-    {name: 'Bulbasaur', height:0.7, types:['grass', 'poison']},
+    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
     {name: 'Charmander', height: 0.6, types: 'fire'},
-    {name:'Pikachu', height:0.4, types:'electric'},
-    {name:'Ponyta', height:1, types:'fire'},
-    ];
+    {name: 'Pikachu', height: 0.4, types: 'electric'},
+    {name: 'Ponyta', height: 1, types: 'fire'},
+];
+
+//DISPLAY NAME AND HEIGHT OF POKEMONS IN DOM
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + ' (height: ' + (pokemonList[i].height) + 'm)');
+
+    if (pokemonList[i].height > 0.8) {
+        document.write(' => Wow, that\'s big!!');
+    }
+
+    document.write('<br>');
+}
+
+
+
