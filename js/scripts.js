fd4a8b204531afcce22a571247ccd8df837fae2e
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-03-04 09:55:18 +0100

    Exchange for-Loop for for-Each-Loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,39 @@
 "use strict";
 
+//
+// let pokemonList = [
+//     {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+//     {name: 'Charmander', height: 0.6, types: 'fire'},
+//     {name: 'Pikachu', height: 0.4, types: 'electric'},
+//     {name: 'Ponyta', height: 1, types: 'fire'},
+// ];
+//
+// //DISPLAY NAME AND HEIGHT OF POKEMONS IN DOM
+// function printArrayDetails () {
+//     for (let i = 0; i < pokemonList.length; i++) {
+//         document.write('<p>' + pokemonList[i].name)
+//
+//         if (pokemonList[i].height){
+//             document.write(' (height: ' + (pokemonList[i].height) + 'm)')
+//         }
+//
+//         if (pokemonList[i].height > 0.8) {
+//             document.write(' => Wow, that\'s big!!');
+//         }
+//
+//         document.write('</p>')
+//     }
+// }
+//
+// printArrayDetails();
+// printArrayDetails();
+// printArrayDetails();
+// printArrayDetails();
+// printArrayDetails();
+// printArrayDetails();
+//
+
+
 let pokemonList = [
     {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
     {name: 'Charmander', height: 0.6, types: 'fire'},
@@ -7,20 +41,12 @@ let pokemonList = [
     {name: 'Ponyta', height: 1, types: 'fire'},
 ];
 
-//DISPLAY NAME AND HEIGHT OF POKEMONS IN DOM
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write('<p>' + pokemonList[i].name)
-
-    if (pokemonList[i].height){
-        document.write(' (height: ' + (pokemonList[i].height) + 'm)')
-    }
-
-    if (pokemonList[i].height > 0.8) {
-        document.write(' => Wow, that\'s big!!');
-    }
-
-    document.write('</p>')
-}
-
-
-
+pokemonList.forEach(function (pokemon) {
+    document.write(
+        `<div> 
+        <h1>${pokemon.name}</h1> 
+        <p>height: ${pokemon.height} m</p> 
+        <p>types: ${pokemon.types} </p> 
+        </div>`
+    )
+})
